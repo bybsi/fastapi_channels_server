a9179528e3b352de47c75785af2b35bd1322e12c
Brian Stull <brain.stull@gmail.com>
2026-01-12 22:34:49 -0500

    add music/activities routes

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-
 from api.routes import channels
-#from app.core.config import settings
+#from api.routes import activities
+#from api.routes import music
 
 api_router = APIRouter()
-#api_router.include_router(login.router)
-#api_router.include_router(users.router)
-#api_router.include_router(utils.router)
+#api_router.include_router(music.router)
+#api_router.include_router(activities.router)
+
+# This is done in app/main.py because it's a websocket route
 #api_router.include_router(channels.router)
 
