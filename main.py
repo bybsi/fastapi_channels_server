a3b9fddbd99610099f5382470a117ef7d6538bd1
Brian Stull <brain.stull@gmail.com>
2026-01-12 22:32:10 -0500

    Cleanup and add some todos

--- main.py
+++ main.py
@@ -7,23 +7,18 @@ from api.routes.channels import websocket_endpoint
 import settings
 
 # TODO run with uvicorn so --ws-max-size can be used.
-app = FastAPI(
- # TODO
- #   title=settings.PROJECT_NAME,
- #   openapi_url=f"{settings.API_V1_STR}/openapi.json",
- #   generate_unique_id_function=custom_generate_unique_id,
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    # TODO on prod
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Set all CORS enabled origins
-if True:#if settings.all_cors_origins:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-app.include_router(api_router) #prefix=settings.API_V1_STR)
-# Must include websocket route independently
+# TODO prefix endpoints with settings.API_V#
+app.include_router(api_router)
+# Must include websocket routes independently
 app.add_api_websocket_route("/ws", websocket_endpoint)
